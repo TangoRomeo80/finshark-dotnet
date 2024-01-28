613cce590b2cfbd13e9bec93d8578a6540806791
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2024-01-28 09:44:27 +0600

    added initial files

---
+++ api/Mappers/StockMapper.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMapper
+    {
+        public static StockDto ToStockDto(this Stock stock){
+            return new StockDto
+            {
+                Id = stock.Id,
+                Symbol = stock.Symbol,
+                CompanyName = stock.CompanyName,
+                Purchase = stock.Purchase,
+                LastDiv = stock.LastDiv,
+                Industry = stock.Industry,
+                MarketCap = stock.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto stockDto)
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDiv = stockDto.LastDiv,
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
+            };
+        }
+
+        public static void UpdateFromUpdateDto(this Stock stock, UpdateStockRequestDto stockDto)
+        {
+            stock.Symbol = stockDto.Symbol;
+            stock.CompanyName = stockDto.CompanyName;
+            stock.Purchase = stockDto.Purchase;
+            stock.LastDiv = stockDto.LastDiv;
+            stock.Industry = stockDto.Industry;
+            stock.MarketCap = stockDto.MarketCap;
+        }
+    }
+}
---
+++ api/Repository/StockRepository.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public Task<List<Stock>> GetAllAsync()
+        {
+            return _context.Stocks.ToListAsync();
+        }
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            return _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockModel)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stock == null)
+            {
+                return null;
+            }
+            stock.Symbol = stockModel.Symbol;
+            stock.CompanyName = stockModel.CompanyName;
+            stock.Purchase = stockModel.Purchase;
+            stock.LastDiv = stockModel.LastDiv;
+            stock.Industry = stockModel.Industry;
+            stock.MarketCap = stockModel.MarketCap;
+
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stock == null)
+            {
+                return null;
+            }
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+    }
+}
