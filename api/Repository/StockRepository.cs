a2ea1fe4c239f07ef654d331263e9b848aebf99f
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2024-02-10 21:31:59 +0600

    Added pagination

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -40,7 +40,10 @@ namespace api.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
 
         }
 
